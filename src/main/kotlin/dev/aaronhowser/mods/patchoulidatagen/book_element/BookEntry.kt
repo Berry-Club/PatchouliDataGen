c9f38195e07c567a0c6d5c696907f9a71d34b0c2
aaron <aaronhowser1@gmail.com>
2025-10-25 17:10:44 -0500

    builder

--- src/main/kotlin/dev/aaronhowser/mods/patchoulidatagen/book_element/BookEntry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/patchoulidatagen/book_element/BookEntry.kt
@@ -4,7 +4,9 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.khanhpham.patchoulidatagen.pages.pagetype.PageType
 import dev.aaronhowser.mods.patchoulidatagen.Util.addIfNotNull
+import net.minecraft.network.chat.Component
 import net.minecraft.world.level.ItemLike
+import java.util.function.Consumer
 
 class BookEntry private constructor(
 	private val saveName: String,
@@ -72,6 +74,87 @@ class BookEntry private constructor(
 		private var readByDefault: Boolean? = null
 		private var sortNum: Int? = null
 		private var turnIn: String? = null
+
+		fun category(category: BookCategory): Builder {
+			this.category = category
+			return this
+		}
+
+		fun setAdvancement(advancement: String): Builder {
+			this.advancement = advancement
+			return this
+		}
+
+		fun setConfigFlag(configFlag: String): Builder {
+			this.configFlag = configFlag
+			return this
+		}
+
+		fun setPriority(priority: Boolean): Builder {
+			this.priority = priority
+			return this
+		}
+
+		fun setSecret(secret: Boolean): Builder {
+			this.secret = secret
+			return this
+		}
+
+		fun setReadByDefault(readByDefault: Boolean): Builder {
+			this.readByDefault = readByDefault
+			return this
+		}
+
+		fun setSortNum(sortNum: Int): Builder {
+			this.sortNum = sortNum
+			return this
+		}
+
+		fun setTurnIn(turnIn: String): Builder {
+			this.turnIn = turnIn
+			return this
+		}
+
+		fun display(entryName: String, icon: ItemLike): Builder {
+			this.name = entryName
+			this.icon = icon
+			return this
+		}
+
+		fun display(entryName: Component, icon: ItemLike): Builder {
+			this.name = entryName.string
+			this.icon = icon
+			return this
+		}
+
+		fun addPage(page: PageType): Builder {
+			this.pages.add(page)
+			return this
+		}
+
+		fun build(consumer: Consumer<BookElement>, saveName: String): BookEntry {
+
+			require(category != null) { "BookEntry $saveName is missing category!" }
+			require(name != null && icon != null) { "BookEntry $saveName is missing display!" }
+
+			val entry = BookEntry(
+				saveName,
+				category!!,
+				name!!,
+				icon!!,
+				pages,
+				advancement,
+				configFlag,
+				priority,
+				secret,
+				readByDefault,
+				sortNum,
+				turnIn
+			)
+
+			consumer.accept(entry)
+			return entry
+		}
 
 		companion object {
 			fun entry(): Builder = Builder()
