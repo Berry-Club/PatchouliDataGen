566647e097cc4caabf2ac8734aaffb97e8b148b3
aaron <aaronhowser1@gmail.com>
2025-10-25 16:12:59 -0500

    progress

--- src/main/kotlin/dev/aaronhowser/mods/patchoulidatagen/book_element/BookCategory.kt
+++ src/main/kotlin/dev/aaronhowser/mods/patchoulidatagen/book_element/BookCategory.kt
@@ -1,6 +1,8 @@
 package dev.aaronhowser.mods.patchoulidatagen.book_element
 
 import com.google.gson.JsonObject
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.ItemLike
 
 class BookCategory private constructor(
@@ -18,15 +20,28 @@ class BookCategory private constructor(
 	override fun toJson(): JsonObject {
 		val json = JsonObject()
 
-		json.addProperty()
+		json.addProperty(TITLE, this.title)
+		json.addProperty(DESCRIPTION, this.description)
+		json.addProperty(ICON, BuiltInRegistries.ITEM.getKey(this.icon.asItem()).toString())
 
+		if (this.sortNum != null) {
+			json.addProperty(SORT_NUM, this.sortNum)
+		}
+
+		json.addProperty(SECRET, this.secret)
+
+		return json
+	}
+
+	fun getCategoryId(): ResourceLocation {
+		return ResourceLocation.fromNamespaceAndPath(header.getBookId(), this.getSaveName())
 	}
 
 	companion object {
-		const val NAME = "name"
+		const val TITLE = "title"
 		const val DESCRIPTION = "description"
 		const val ICON = "icon"
 		const val SORT_NUM = "sort_num"
-		const val SECRET
+		const val SECRET = "secret"
 	}
 }
