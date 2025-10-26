b648ef76a4e84e68724807808ca68f4ca26e1ec9
aaron <aaronhowser1@gmail.com>
2025-10-25 19:01:16 -0500

    Refactor PatchouliBookProvider: Update imports and improve code formatting

--- src/main/java/com/khanhpham/patchoulidatagen/provider/PatchouliBookProvider.java
+++ src/main/java/com/khanhpham/patchoulidatagen/provider/PatchouliBookProvider.java
@@ -2,18 +2,19 @@ package com.khanhpham.patchoulidatagen.provider;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.khanhpham.patchoulidatagen.bookelement.BookCategory;
-import com.khanhpham.patchoulidatagen.bookelement.BookElement;
-import com.khanhpham.patchoulidatagen.bookelement.BookEntry;
-import com.khanhpham.patchoulidatagen.bookelement.BookHeader;
-import net.minecraft.data.*;
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookCategory;
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookElement;
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookEntry;
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookHeader;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.pathfinder.Target;
-import net.minecraftforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,71 +25,81 @@ import java.util.function.Consumer;
  * @see net.minecraft.data.advancements.AdvancementProvider
  */
 public abstract class PatchouliBookProvider implements DataProvider {
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    private final DataGenerator generator;
-    @Nullable
-    private final ExistingFileHelper fileHelper;
-    protected final String modid;
-    private final String bookName;
+	private final DataGenerator generator;
+	@Nullable
+	private final net.neoforged.neoforge.common.data.ExistingFileHelper fileHelper;
+	protected final String modid;
+	private final String bookName;
 
-    public PatchouliBookProvider(DataGenerator generator, @Nullable ExistingFileHelper fileHelper,
-            String bookName, String modid) {
-        this.fileHelper = fileHelper;
-        this.generator = generator;
-        this.modid = modid;
-        this.bookName = bookName;
-    }
+	public PatchouliBookProvider(
+			DataGenerator generator, @Nullable ExistingFileHelper fileHelper,
+			String bookName, String modid
+	) {
+		this.fileHelper = fileHelper;
+		this.generator = generator;
+		this.modid = modid;
+		this.bookName = bookName;
+	}
 
-    public PatchouliBookProvider(DataGenerator generator, String bookName, String modid) {
-        this(generator, null, bookName, modid);
-    }
+	public PatchouliBookProvider(DataGenerator generator, String bookName, String modid) {
+		this(generator, null, bookName, modid);
+	}
 
-    @Override
-    public @NotNull CompletableFuture<?> run(CachedOutput pOutput) {
-        PackOutput dataFolder = generator.getPackOutput();
-        Set<String> bookLocations = new HashSet<>();
-        final String bookDefaultPath = "data/" + modid + "/patchouli_books/" + bookName + "/en_us/";
-        Consumer<BookElement> elementConsumer = ((element) -> {
-            if (bookLocations.add(element.getSaveName())) {
-                if (element instanceof BookEntry entries) {
-                    Path entryFolder = resolvePath(dataFolder,
-                            bookDefaultPath + "entries/" + entries.getSaveName() + ".json");
-                    this.saveData(gson, pOutput, entries, entryFolder);
-                } else if (element instanceof BookCategory category) {
-                    Path categoryFolder = resolvePath(dataFolder,
-                            bookDefaultPath + "categories/" + category.getSaveName() + ".json");
-                    this.saveData(gson, pOutput, category, categoryFolder);
-                } else if (element instanceof BookHeader header) {
-                    Path headerFolder = resolvePath(dataFolder,
-                            "data/" + modid + "/patchouli_books/" + bookName + "/book.json");
-                    this.saveData(gson, pOutput, header, headerFolder);
-                }
+	@Override
+	public @NotNull CompletableFuture<?> run(CachedOutput pOutput) {
+		PackOutput dataFolder = generator.getPackOutput();
+		Set<String> bookLocations = new HashSet<>();
+		final String bookDefaultPath = "data/" + modid + "/patchouli_books/" + bookName + "/en_us/";
+		Consumer<BookElement> elementConsumer = ((element) -> {
+			if (bookLocations.add(element.getSaveName())) {
+				if (element instanceof BookEntry entries) {
+					Path entryFolder = resolvePath(
+							dataFolder,
+							bookDefaultPath + "entries/" + entries.getSaveName() + ".json"
+					);
+					this.saveData(gson, pOutput, entries, entryFolder);
+				} else if (element instanceof BookCategory category) {
+					Path categoryFolder = resolvePath(
+							dataFolder,
+							bookDefaultPath + "categories/" + category.getSaveName() + ".json"
+					);
+					this.saveData(gson, pOutput, category, categoryFolder);
+				} else if (element instanceof BookHeader header) {
+					Path headerFolder = resolvePath(
+							dataFolder,
+							"data/" + modid + "/patchouli_books/" + bookName + "/book.json"
+					);
+					this.saveData(gson, pOutput, header, headerFolder);
+				}
 
-            } else {
-                ResourceLocation bookLocation = new ResourceLocation(modid, element.getSaveName());
-                throw new IllegalStateException("Duplicate book page [" + bookLocation + "]");
-            }
-        });
+			} else {
+				ResourceLocation bookLocation = ResourceLocation.fromNamespaceAndPath(modid, element.getSaveName());
+				throw new IllegalStateException("Duplicate book page [" + bookLocation + "]");
+			}
+		});
 
-        return CompletableFuture.runAsync(() -> {
-            buildPages(elementConsumer);
-        });
-    }
+		return CompletableFuture.runAsync(() -> {
+			buildPages(elementConsumer);
+		});
+	}
 
-    private Path resolvePath(PackOutput path, String pathOther) {
-        return path.getOutputFolder().resolve(pathOther);
-    }
+	private Path resolvePath(PackOutput path, String pathOther) {
+		return path.getOutputFolder().resolve(pathOther);
+	}
 
-    private <T extends BookElement> void saveData(Gson gson, CachedOutput cache, T jsonObject,
-            Path bookElementPath) {
-        DataProvider.saveStable(cache, jsonObject.toJson(), bookElementPath);
-    }
+	private <T extends BookElement> void saveData(
+			Gson gson, CachedOutput cache, T jsonObject,
+			Path bookElementPath
+	) {
+		DataProvider.saveStable(cache, jsonObject.toJson(), bookElementPath);
+	}
 
-    protected abstract void buildPages(Consumer<BookElement> consumer);
+	protected abstract void buildPages(Consumer<BookElement> consumer);
 
-    @Override
-    public @NotNull String getName() {
-        return "Patchouli Book: " + bookName.toLowerCase();
-    }
+	@Override
+	public @NotNull String getName() {
+		return "Patchouli Book: " + bookName.toLowerCase();
+	}
 }
---
+++ src/main/kotlin/dev/aaronhowser/mods/patchoulidatagen/provider/PatchouliBookProvider.kt
@@ -0,0 +1,103 @@
+package dev.aaronhowser.mods.patchoulidatagen.provider
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookCategory
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookElement
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookEntry
+import dev.aaronhowser.mods.patchoulidatagen.book_element.BookHeader
+import net.minecraft.data.CachedOutput
+import net.minecraft.data.DataGenerator
+import net.minecraft.data.DataProvider
+import net.minecraft.data.PackOutput
+import net.minecraft.resources.ResourceLocation
+import net.neoforged.neoforge.common.data.ExistingFileHelper
+import java.nio.file.Path
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+
+abstract class PatchouliBookProvider(
+	private val generator: DataGenerator,
+	private val existingFileHelper: ExistingFileHelper?,
+	private val bookName: String,
+	private val modId: String
+) : DataProvider {
+
+	private val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+
+	constructor(
+		generator: DataGenerator,
+		bookName: String,
+		modId: String
+	) : this(generator, null, bookName, modId)
+
+	override fun getName(): String {
+		return "Patchouli Book: $bookName"
+	}
+
+	override fun run(output: CachedOutput): CompletableFuture<*> {
+		val dataFolder = generator.packOutput
+
+		val bookLocations = mutableSetOf<String>()
+		val bookDefaultPath = "data/$modId/patchouli_books/$bookName/en_us"
+
+		val elementConsumer: Consumer<BookElement> = Consumer { element ->
+			val addedSuccessfully = bookLocations.add(element.getSaveName())
+
+			if (addedSuccessfully) {
+				val rl = ResourceLocation.fromNamespaceAndPath(modId, element.getSaveName())
+				error("Duplicate book page [$rl]")
+			}
+
+			when (element) {
+				is BookEntry -> {
+					val entryFolder = resolvePath(
+						dataFolder,
+						"$bookDefaultPath/entries/${element.getSaveName()}.json"
+					)
+
+					saveData(gson, output, element, entryFolder)
+				}
+
+				is BookCategory -> {
+					val categoryFolder = resolvePath(
+						dataFolder,
+						"$bookDefaultPath/categories/${element.getSaveName()}.json"
+					)
+
+					saveData(gson, output, element, categoryFolder)
+				}
+
+				is BookHeader -> {
+					val headerFolder = resolvePath(
+						dataFolder,
+						"data/$modId/patchouli_books/$bookName/book.json"
+					)
+
+					saveData(gson, output, element, headerFolder)
+				}
+			}
+
+		}
+
+		return CompletableFuture.runAsync {
+			buildPages(elementConsumer)
+		}
+	}
+
+	private fun <T : BookElement> saveData(
+		gson: Gson,    // unused currently
+		cache: CachedOutput,
+		bookElement: T,
+		bookElementPath: Path
+	) {
+		DataProvider.saveStable(cache, bookElement.toJson(), bookElementPath)
+	}
+
+	private fun resolvePath(path: PackOutput, pathOther: String): Path {
+		return path.outputFolder.resolve(pathOther)
+	}
+
+	abstract fun buildPages(consumer: Consumer<BookElement>)
+
+}
